04ec4e1f9c2feb7c340dfb42303500f64cb5b5fb
Mark S <stcoronawraps@gmail.com>
2023-01-09 13:59:54 -0700

    updated Project URLS + Created First View Function

--- products/models.py
+++ products/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=800)
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(decimal_places=2, max_digits=8)
     inventory_quantity = models.IntegerField()
