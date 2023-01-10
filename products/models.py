afde9367d583a0e7f4cef7c76373152a3ea3ec2e
Mark S <stcoronawraps@gmail.com>
2023-01-09 21:27:10 -0700

    Added Serializers + Updated Views + Updated URLS + Updated Models + Fixed Multiple Syntax Errors.

--- products/models.py
+++ products/models.py
@@ -5,4 +5,4 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=800)
     price = models.DecimalField(decimal_places=2, max_digits=8)
-    inventory_quantity = models.IntegerField()
+    inventory_quantity = models.IntegerField(blank=True, null=True)
